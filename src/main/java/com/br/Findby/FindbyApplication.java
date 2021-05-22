95f7d3e5ba75ecf6cd331631cdb20d81b342e5fa
Maycon W Soler <mayconwsoler@gmail.com>
2021-05-22 17:45:27 -0300

    Adicionando o projeto ao remoto

---
+++ src/main/java/com/br/Findby/FindbyApplication.java
@@ -0,0 +1,13 @@
+package com.br.Findby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindbyApplication.class, args);
+	}
+
+}
