5c781f0a741311d399be8c1140e164ea7e77a4b0
Maycon W Soler <mayconwsoler@gmail.com>
2021-05-26 19:59:41 -0300

    Adicionando permissão de Cross

--- src/main/java/com/br/Findby/Controller/SegmentoController.java
+++ src/main/java/com/br/Findby/Controller/SegmentoController.java
@@ -3,6 +3,7 @@ package com.br.Findby.Controller;
 import com.br.Findby.Model.Segmento;
 import com.br.Findby.Service.SegmentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class SegmentoController {
 
     @Autowired
     private SegmentoService segmentoService;
+    @CrossOrigin(origins = "*")
     @GetMapping
     public List<Segmento> listar(){
         return segmentoService.listarSegmentos();
